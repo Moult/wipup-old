8ed4543d74e039ca9c7dc1a873e2fbe446677820
Dion Moult <dion@thinkmoult.com>
2012-11-09 17:35:40 +1100

    Add basic validation driver.

---
+++ application/classes/Driver/Validation.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Eadrax application/classes/Driver/Validation.php
+ *
+ * @package   Driver
+ * @author    Dion Moult <dion@thinkmoult.com>
+ * @copyright (c) 2012 Dion Moult
+ * @license   ISC http://opensource.org/licenses/isc-license.txt
+ * @link      http://wipup.org/
+ */
+
+defined('SYSPATH') OR die('No direct script access.');
+
+/**
+ * This is a driver for KO's built in validation.
+ *
+ * @package    Driver
+ * @subpackage Validation
+ */
+class Driver_Validation implements Eadrax\Eadrax\Entity\Validation
+{
+    private $instance;
+
+    public function setup(array $input_data)
+    {
+        $this->instance = Validation::factory($input_data);
+    }
+
+    public function rule($key, $rule, $arg = NULL)
+    {
+        if ($arg !== NULL)
+        {
+            $this->instance->rule($key, $rule, array(':value', $arg));
+        }
+        else
+        {
+            $this->instance->rule($key, $rule);
+        }
+    }
+
+    public function callback($key, array $function)
+    {
+        $this->instance->rule($key, $function);
+    }
+
+    public function check()
+    {
+        return $this->instance->check();
+    }
+
+    public function errors()
+    {
+        return $this->instance->errors();
+    }
+}
